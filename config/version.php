dcd3c2ade5c1345b89c09993c18d3fa77444d487
Sabih <sabih@thedatech.com>
2021-09-19 18:02:20 +0500

    first commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Document',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '1',
+
+    'patch'     =>  '25',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '10-September-2021',
+
+    'time'      =>  '01:00',
+
+    'zone'      =>  'GMT +3',
+
+];
